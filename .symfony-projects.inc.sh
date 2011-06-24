3263c41a1e1a03d2397940fcea6b08270bc4c719
Matt Smith <mcs@darkregion.net>
2011-06-23 21:43:59 -0500

    made numerous improvements

--- .symfony-projects.inc.sh
+++ .symfony-projects.inc.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-# Define the following variables:
-PATH_TO_SF_PROJECTS=$HOME/sites/	# keep trailing slash
+### Define the following variables:
+PATH_TO_SF_PROJECTS="$HOME/sites/"	# keep trailing slash
 
-# Do not touch beyond this line:
-OLD_PWD=$PWD
-NEW_PWD=$(echo $PWD | sed -r "s;($PATH_TO_SF_PROJECTS)([^/]+)(.*);\1\2;")	# current $PWD is transformed into "$PATH_TO_SF_PROJECTS/<project dir>"
+
+### Do not touch beyond this line:
+##
+# The current $PWD is transformed into "$PATH_TO_SF_PROJECTS/<project dir>".
+#
+SF_PROJECT_PATH="$(echo "$PWD" | sed -r "s;("$PATH_TO_SF_PROJECTS")([^/]+)(.*);\1\2;")"
+
+##
+# Go ahead and change into the project directory.
+#
+cd "$SF_PROJECT_PATH"
